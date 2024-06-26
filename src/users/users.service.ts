1960cd41dd87e55e919aacc68f99a865e28a354f
mallikarjunkamtam <mallikarjunkamtam@gmail.com>
2024-06-26 05:41:24 +0530

    implementation of auth

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const newUser = this.usersRepository.create({
+      ...createUserDto,
+      password_hash: hashedPassword,
+    });
+    return this.usersRepository.save(newUser);
+  }
+
+
+ 
+}
