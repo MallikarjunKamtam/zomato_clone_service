cec5d10d4eb0f6e2aa76d6b9d1ef48d85f5e107d
mallikarjunkamtam <mallikarjunkamtam@gmail.com>
2024-06-26 07:12:27 +0530

    changes in configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,29 @@
+//app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'mallikarjunkamtam',
-      password: 'postgres',
-      database: 'zomato_clone',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+      }),
     }),
     AuthModule,
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,30 @@ import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TypeOrmExceptionFilter } from './common/filters/typeorm-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
- 
-  app.useGlobalPipes(new ValidationPipe({
-    exceptionFactory: (errors) => {
-      const errorMessages = errors.map(
-        (error) => `${error.property} - ${Object.values(error.constraints).join(', ')}`
-      );
-      return new BadRequestException(errorMessages);
-    },
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const errorMessages = errors.map(
+          (error) =>
+            `${error.property} - ${Object.values(error.constraints).join(', ')}`,
+        );
+        return new BadRequestException(errorMessages);
+      },
+    }),
+  );
 
-  
   app.useGlobalFilters(new TypeOrmExceptionFilter());
 
-  await app.listen(4000); // TODO : change port to config
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port, () => {
+    console.log(`SERVER STARTED AT PORT ${port}.....`);
+  });
 }
 bootstrap();
